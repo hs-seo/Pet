afd1e3cea331a1239a604b04da22cd6bf77c2c3b
hs <pcnshs@gmail.com>
2016-07-23 21:47:50 +0900

    마이그레이션 파일과 모델 파일 작성. User, Post, Comment 테이블 만들기

---
+++ app/models/post.rb
@@ -0,0 +1,8 @@
+class Post < ActiveRecord::Base
+  belongs_to  :user
+  has_many  :comments
+  validates :category, :inclusion => { :in => ["예방접종","중성화","미용","일반진료"], :message => "예방접종, 중성화, 미용, 일반진료 중 하나를 선택하셔야 합니다." }
+  validates :title, :presence => { :message => "제목을 반드시 입력하셔야 합니다." }
+end
+
+end
