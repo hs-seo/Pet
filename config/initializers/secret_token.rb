1c3f4d4d2970c8ea4324e686d3905330a0caf645
hs <pcnshs@gmail.com>
2016-07-23 21:06:14 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pet::Application.config.secret_key_base = 'e8491b669f0a067eb80b79ae71bc1d7da0adce0241ccebe57c88628cbca709a9a9f3efbae15190529196c54ac7028e401348ee3c6ab513165b03db9222ef7dcc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pet::Application.config.session_store :cookie_store, key: '_pet_session'
