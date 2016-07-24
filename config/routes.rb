678b8a456802ab565bf316c6ac10ee1f9b41ba3b
hs <pcnshs@gmail.com>
2016-07-24 17:17:24 +0900

    로그인 기능 추가하기

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Pet::Application.routes.draw do
   get "users/signup"
   post "users/signup_complete"
   get "users/login"
-  get "users/login_complete"
+  post "users/login_complete"
   get "users/logout_complete"
   root 'treatments#posts'
   get "/:category" => 'treatments#posts_category'
