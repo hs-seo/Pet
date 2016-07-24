d02e666d9921ee1ac6df2117e2733d76f97a3c21
hs <pcnshs@gmail.com>
2016-07-24 18:04:27 +0900

    carrierwave gem 사용 준비하기

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ActiveRecord::Base
   has_many  :comments
   validates :category, :inclusion => { :in => ["예방접종","중성화","미용","일반진료"], :message => "예방접종, 중성화, 미용, 일반진료 중 하나를 선택하셔야 합니다." }
   validates :title, :presence => { :message => "제목을 반드시 입력하셔야 합니다." }
+	mount_uploader :image, ImageUploader
 end
